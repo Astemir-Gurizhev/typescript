c16284b638cbc7dc4d36806b07f8111ce42cd28a
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-14 15:25:10 +0300

    generic

--- src/App.tsx
+++ src/App.tsx
@@ -199,7 +199,7 @@ function App() {
   const inputElement = document.querySelector("input");
   const value1 = (inputElement as HTMLInputElement).value;
   // const value2 = (<HTMLInputElement>inputElement).value (2 вариант)
-
+  
   //===============================================================================
   return <></>;
 }
